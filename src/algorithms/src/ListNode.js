67bd369b6f0533729242460323852c121785edad
yuetianxiang <979054461@qq.com>
2020-10-03 23:31:29 +0800

    implement ListNode

--- src/algorithms/src/ListNode.js
+++ src/algorithms/src/ListNode.js
@@ -6,70 +6,137 @@ class ListNode {
         this.value = (value === undefined ? 0 : value);
         this.next = (next === undefined ? null : next);
     }
+}
 
-    /**
-     * 头插法
-     * @param  {...any} args 
-     * @returns {ListNode}
-     */
-    ListInitHeadInsert(...args){
+/**
+ * 头插法
+ * @param  {...any} args 
+ * @returns {ListNode}
+ */
+function ListInitHeadInsert(...args) {
+    const head = new ListNode();
+    let p = head;
+    args.forEach(item => {
+        let node = new ListNode(item)
+        let tmp = p.next
+        p.next = node
+        node.next = tmp
+    })
+    return head;
+}
+/**
+ * 尾插法
+ * @param  {...any} args
+ * @returns {ListNode} 
+ */
+function ListInitTailInsert(...args) {
+    const head = new ListNode();
+    let p = head;
+    args.forEach(item => {
+        p.next = new ListNode(item, null)
+        p = p.next
+    })
+    return head;
+}
+/**
+ * 按序号查找节点
+ * @param {ListNode} head
+ * @param {Integer} i 
+ * @returns {ListNode}
+ */
+function GetElem(head, i) {
+    if (head === null) return null;
 
+    let p = head;
+    let j = 0;
+    while (p && j < i) {
+        p = p.next
+        j++
     }
-    /**
-     * 尾插法
-     * @param  {...any} args
-     * @returns {ListNode} 
-     */
-    ListInitTailInsert(...args){
+    return p
+}
 
+/**
+ * 按值查找节点
+ * @param {ListNode} head
+ * @param {any} value 
+ * @returns {ListNode}
+ */
+function LocateElem(head, value) {
+    if (head === null) return null;
+
+    let p = head.next;
+    while (p.value !== value) {
+        p = p.next
     }
-    /**
-     * 按序号查找节点
-     * @param {Integer} i 
-     * @returns {ListNode}
-     */
-    GetElem(i){
+    return p
+}
+/**
+ * 当前节点之后插入
+ * @param {ListNode} node 
+ * @param {any} value
+ * @returns {Boolean} 
+ */
+function InsertNodeAfter(node, value) {
+    if (head === null) return false;
+    const p = new ListNode(value)
+    const tmp = node.next
+    node.next = p
+    p.next = tmp
+    return true
 
+}
+/**
+ * 当前节点之前插入
+ * @param {ListNode} node
+ * @returns {Boolean} 
+ */
+function InsertNodeBefore(node, value) {
+    if (head === null) return false;
+    const p = new ListNode(node.value);
+    node.value = value
+
+    const tmp = node.next
+    node.next = p
+    p.next = tmp
+    return true
+}
+/**
+ * 删除位置i处的节点
+ * @param {ListNode} head
+ * @param {Integer} i 
+ * @returns {ListNode}
+ */
+function DeleteNode(head, i) {
+    if (head === null) return null;
+    if (i <= 0) return null;
+    let p = head
+    let j = 0
+    while (p && j < i - 1) {
+        p = p.next
+        j++
+    }
+    if (p) {
+        let ret = p.next;
+        p.next = ret ? ret.next : null;
+        return ret
+    } else {
+        return null
     }
 
-    /**
-     * 按值查找节点
-     * @param {any} value 
-     * @returns {ListNode}
-     */
-    LocateElem(value){
-
+}
+/**
+ * 求表长
+ * @param {ListNode} head
+ */
+function size(head) {
+    let ret = 0
+    let p = head ? head.next : null;
+    while (p) {
+        p = p.next
+        ret++
     }
-    /**
-     * 当前节点之后插入
-     * @param {ListNode} node 
-     * @returns {Boolean} 
-     */
-    InsertNodeAfter(node){
-
-    }
-    /**
-     * 当前节点之前插入
-     * @param {ListNode} node
-     * @returns {Boolean} 
-     */
-    InsertNodeBefore(node){
-
-    }
-    /**
-     * 删除位置i处的节点
-     * @param {Integer} i 
-     * @returns {ListNode}
-     */
-    DeleteNode(i){
-
-    }
-    /**
-     * 求表长
-     */
-    size(){
-
-    }
+    return ret
 }
 
 export default ListNode;
