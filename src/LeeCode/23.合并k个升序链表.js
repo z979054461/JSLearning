6f0b97b9027d1a7b41cf624821854f56d5116e0b
yuetianxiang <979054461@qq.com>
2020-10-24 20:55:45 +0800

    LeeCode 23 & 24

---
+++ src/LeeCode/23.合并k个升序链表.js
@@ -0,0 +1,105 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=javascript
+ *
+ * [23] 合并K个升序链表
+ *
+ * https://leetcode-cn.com/problems/merge-k-sorted-lists/description/
+ *
+ * algorithms
+ * Hard (53.39%)
+ * Likes:    964
+ * Dislikes: 0
+ * Total Accepted:    180.9K
+ * Total Submissions: 338.5K
+ * Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+ *
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ * 
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+ * 输出：[1,1,2,3,4,4,5,6]
+ * 解释：链表数组如下：
+ * [
+ * ⁠ 1->4->5,
+ * ⁠ 1->3->4,
+ * ⁠ 2->6
+ * ]
+ * 将它们合并到一个有序链表中得到。
+ * 1->1->2->3->4->4->5->6
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：lists = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：lists = [[]]
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * k == lists.length
+ * 0 <= k <= 10^4
+ * 0 <= lists[i].length <= 500
+ * -10^4 <= lists[i][j] <= 10^4
+ * lists[i] 按 升序 排列
+ * lists[i].length 的总和不超过 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists) {
+    if (lists.length < 1) return null;
+    if (lists.length === 1) return lists[0];
+
+    const hair = new ListNode(0, lists.shift());
+    let p = hair, q, r;
+    while (lists.length) {
+        q = lists.shift();
+        while (q) {
+            if (p.next === null) {
+                p.next = q;
+                break;
+            } else if (q.val <= p.next.val) {
+                r = q.next;
+                q.next = p.next;
+                p.next = q;
+                q = r;
+            } else {
+                p = p.next;
+            }
+        }
+        p = hair;
+    }
+    return hair.next
+};
+// @lc code=end
+
+
+// @after-stub-for-debug-begin
+module.exports = mergeKLists;
+// @after-stub-for-debug-end
---
+++ src/LeeCode/24.两两交换链表中的节点.js
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=javascript
+ *
+ * [24] 两两交换链表中的节点
+ *
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (68.19%)
+ * Likes:    722
+ * Dislikes: 0
+ * Total Accepted:    188K
+ * Total Submissions: 275.4K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * 
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：head = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+    const hair = new ListNode(-1, head);
+    let p = hair, q = p.next, r;
+    while (q && q.next) {
+        //删除q
+        p.next = q.next;
+        p = p.next;
+        if (p.next) {
+            //插入q
+            r = p.next;
+            p.next = q
+            q.next = r
+            //pq前移
+            p = q
+            q = q.next
+        } else {
+            q.next = null;
+            p.next = q
+        }
+    }
+    return hair.next
+};
+// @lc code=end
+
+
+// @after-stub-for-debug-begin
+module.exports = swapPairs;
+// @after-stub-for-debug-end
