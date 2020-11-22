6033133b536c3de22958f9521c40175fa6864e72
yuetianxiang <979054461@qq.com>
2020-11-22 22:03:25 +0800

    add mysql安装步骤 view-koa

--- docs/documents/README.md
+++ docs/documents/README.md
@@ -9,3 +9,5 @@ location: HangZhou
 此目录用于存放文档
 
 @TODO  多行字符串怎么实现KMP算法？
+@TODO   CI/CD
+@TODO  giuhub tag https://www.jianshu.com/p/36202c29e6ae
---
+++ src/NodeDemo/view-koa/app.js
@@ -0,0 +1,43 @@
+const Koa = require('koa');
+const path = require('path')
+const bodyParser = require('koa-bodyparser');
+
+const controller = require('./controller');
+
+const templating = require('./templating');
+
+const app = new Koa();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    var
+        start = new Date().getTime(),
+        execTime;
+    await next();
+    execTime = new Date().getTime() - start;
+    ctx.response.set('X-Response-Time', `${execTime}ms`);
+});
+
+// static file support:
+if (! isProduction) {
+    let staticFiles = require('./static-files');
+    app.use(staticFiles('/bootstrap/dist/', __dirname+'/node_modules/bootstrap/dist'));
+}
+
+// parse request body:
+app.use(bodyParser());
+
+// add nunjucks as view:
+app.use(templating(path.resolve(__dirname, 'views'), {
+    noCache: !isProduction,
+    watch: !isProduction
+}));
+
+// add controller:
+app.use(controller());
+
+app.listen(3000);
+console.log('app started at port 3000...');
