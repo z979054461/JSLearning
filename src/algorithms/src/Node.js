82a78b70118a58966e3a8d916c48258caf631614
yuetianxiang <979054461@qq.com>
2020-12-20 23:13:55 +0800

    完成数据结构-线性表章节  增加目录占位

--- src/algorithms/src/Node.js
+++ src/algorithms/src/Node.js
@@ -1,3 +1,4 @@
+// #region ListNode
 /**
  * Definition for singly-linked list.
  */
@@ -7,10 +8,11 @@ export class ListNode {
         this.next = (next === undefined ? null : next);
     }
 }
+// #endregion ListNode
 
+// #region DoublyListNode
 /**
  * Definition for double-linked list.
- * @todo
  */
 export class DoublyListNode extends ListNode {
     constructor(value, next, prev) {
@@ -18,9 +20,23 @@ export class DoublyListNode extends ListNode {
         this.prev = null;//前驱指针
     }
 }
+// #endregion DoublyListNode
 
+// #region StaticListNode
+/**
+ * Definition for static-linked list.
+ * 使用数组下标作为指针域
+ */
+export class StaticListNode {
+    constructor(value, next) {
+        this.value = (value === undefined ? 0 : value);
+        this.next = (next === undefined ? -1 : next);
+    }
+}
+// #endregion StaticListNode
 
 export default {
     ListNode,
     DoublyListNode,
+    StaticListNode,
 }
