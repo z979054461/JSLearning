b7c220dd162101cb72b80ed6bc036241663a4ce6
YueTianxiang <979054461@qq.com>
2021-01-23 15:15:18 +0800

    LeeCode 138补充其他方法

--- src/LeeCode/138.复制带随机指针的链表.js
+++ src/LeeCode/138.复制带随机指针的链表.js
@@ -44,6 +44,71 @@ var copyRandomList = function(head) {
     }
     return nodeList[0];
 };
+/**
+ * @param {Node} head
+ * @return {Node}
+ * 优化一下，使用map存储对应关系  98%
+ */
+var copyRandomList = function (head) {
+    if (head === null) return null;
+    const indexMap = new Map();//存储原始节点的下标
+    let p = head,
+        i = 0, tmpNode;
+    let nodeArr = [];
+    //创建节点，维护val值
+    while (p) {
+        tmpNode = new Node(p.val, null, p.random);
+        nodeArr.push(tmpNode);
+        indexMap.set(p, i++);
+        p = p.next;
+    }
+    //维护next指针和random指针
+    let randomIdx;//原始随机节点的下标
+    nodeArr.forEach((node, index) => {
+        randomIdx = indexMap.get(node.random);
+        node.next = nodeArr[index + 1] || null;
+        node.random = nodeArr[randomIdx] || null;
+    })
+    return nodeArr[0]
+};
+/**
+ * @param {Node} head
+ * @return {Node}
+ * 进一步优化，遍历一次
+ */
+var copyRandomList = function (head) {
+    if (head === null) return null;
+    const nodeMap = new Map();//存储原始节点和新节点的对应关系
+    let p = head,
+        tmpNode;
+    let hair = new Node(),//头结点
+        tail = hair,//尾指针
+        randomNode;
+    //尾插法创建节点，维护当前节点的val、random和上一节点的next
+    while (p) {
+        if (!p.random) {
+            randomNode = null;
+        } else if (nodeMap.has(p.random)) {
+            randomNode = nodeMap.get(p.random);
+        } else {
+            randomNode = new Node(p.random.val);
+            nodeMap.set(p.random, randomNode);
+        }
+        tmpNode = nodeMap.has(p) ? nodeMap.get(p) : new Node(p.val);
+        tmpNode.random = randomNode;
+        nodeMap.set(p, tmpNode);
+        tail.next = tmpNode;
+        tail = tmpNode;
+        p = p.next;
+    }
+    return hair.next;
+};
+
+// 有个很清奇的思路（官方方法三）：
+// 每个原始节点后面复制一个节点，新节点random指针就是旧节点random.next,最后再将新旧节点拆分。
+// 可以看这个解释加强理解：https://leetcode-cn.com/problems/copy-list-with-random-pointer/solution/javatu-jie-cong-hashmapdao-chang-shu-kong-jian-by-/
+
+
 // @lc code=end
 
 
