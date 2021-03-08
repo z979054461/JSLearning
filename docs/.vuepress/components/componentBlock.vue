e02ed19fe9ac690ae00c0080e43bc1a5ef3b0016
YueTianxiang <979054461@qq.com>
2021-03-08 16:44:20 +0800

    HTML5 标签列表

---
+++ docs/.vuepress/components/componentBlock.vue
@@ -0,0 +1,35 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th scope="col" v-for="item in header" :key="item">
+                    {{ item }}
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(i, idx) in gridData" :key="i + idx">
+                <td v-for="(j, idx) in i" :key="j + idx" v-text="j"></td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: 'componentBlock',
+    props: {
+        header: {
+            type: Array,
+            default: () => []
+        },
+        gridData: {
+            type: Array,
+            default: () => []
+        },
+    },  
+}
+</script>
+
+<style>
+</style>
