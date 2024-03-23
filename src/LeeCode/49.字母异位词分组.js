77685626a2a7c4d2f4099503a72bc18f6c2fe8bd
z979054461 <979054461@qq.com>
2024-03-23 16:24:25 +0800

    feat(leecode): 49.字母异位词分组

---
+++ src/LeeCode/49.字母异位词分组.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=49 lang=javascript
+ *
+ * [49] 字母异位词分组
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  const map = new Map()
+
+  for (let str of strs) {
+    const key = str.split('').sort().join()
+    map.set(key, map.has(key) ? [...map.get(key), str] : [str])
+  }
+
+  return [...map.values()]
+}
+// @lc code=end
